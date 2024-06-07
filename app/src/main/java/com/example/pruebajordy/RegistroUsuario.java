2c187b6ef061fe89da697f7699f4a1372ee1906f
valerycanches <74372171+valerycanches@users.noreply.github.com>
2024-06-07 12:24:49 -0500

    registro usuario

--- app/src/main/java/com/example/pruebajordy/RegistroUsuario.java
+++ app/src/main/java/com/example/pruebajordy/RegistroUsuario.java
@@ -1,8 +1,6 @@
 package com.example.pruebajordy;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistroUsuario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro_usuario);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void Registrar (View view) {
-        Intent i = new Intent(this,RegistroUsuario.class);
-
-        startActivity(i);
-        finish();
     }
 }
