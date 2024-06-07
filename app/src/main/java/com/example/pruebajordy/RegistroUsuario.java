367b9f1769a46a9c6db48d50261d77d9a6ff9fbe
valerycanches <74372171+valerycanches@users.noreply.github.com>
2024-06-07 15:03:21 -0500

    boton volver

--- app/src/main/java/com/example/pruebajordy/RegistroUsuario.java
+++ app/src/main/java/com/example/pruebajordy/RegistroUsuario.java
@@ -1,6 +1,8 @@
 package com.example.pruebajordy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class RegistroUsuario extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void Volver(View view){
+        Intent i = new Intent(this, MainActivity.class);
+
+        startActivity(i);
+        finish();v
     }
 }
